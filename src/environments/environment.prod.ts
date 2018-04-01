dd2a0f05c6bf0f1cf76fadb19cbb74164601dd96
h-p-r <dev.ac.hpr@gmail.com>
2018-04-01 23:49:42 +0530

    api fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyBigsw_2kYKwtmYtpxfl-PqjCednrLmdGg",
-    authDomain: "ngcrud-65a02.firebaseapp.com",
-    databaseURL: "https://ngcrud-65a02.firebaseio.com",
-    projectId: "ngcrud-65a02",
-    storageBucket: "",
-    messagingSenderId: "639390030567"
-  }
+  
 };
